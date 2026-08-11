70dbb8efa4c97cf4a106ae05afb4c783139aa53d
Tom ALTHAUS <berstheim67@gmail.com>
2026-08-11 15:20:30 +0200

    Fix lang choice (space)

--- install.sh
+++ install.sh
@@ -173,7 +173,7 @@ if [ "$language" == "" ]; then language="fr"
 fi
 
 sudo localectl set-x11-keymap "$language"
-echo "hl.config({input = {kb_layout  = \"${language}\", kb_variant = \"\", kb_model   = \"\", kb_options = \"\", kb_rules   = \"\", follow_mouse = 1, sensitivity = 0, repeat_rate = 50, repeat_delay = 200, numlock_by_default = true, touchpad = { natural_scroll = true, }, }, })" > ~/.config/hypr/input.lua
+echo "hl.config({input = {kb_layout = \"${language}\", kb_variant = \"\", kb_model = \"\", kb_options = \"\", kb_rules = \"\", follow_mouse = 1, sensitivity = 0, repeat_rate = 50, repeat_delay = 200, numlock_by_default = true, touchpad = { natural_scroll = true, }, }, })" > ~/.config/hypr/input.lua
 
 echo "Installation terminée!"
 echo "Il est fortement recommandé de redémarrer votre appareil avant de poursuivre!"
